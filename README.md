e694948471b5df5fcabcdaf8482be8dc73fe189f
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2023-12-04 20:39:58 +0800

    support single number 0 (#2)

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 [![CI](https://github.com/mo-xiaoming/life-lang/actions/workflows/CI.yaml/badge.svg)](https://github.com/mo-xiaoming/life-lang/actions/workflows/CI.yaml)
 
-[//]: # (Following Julian Hartl's youtube series [_Building a compiler in rust_](https://github.com/julian-hartl/fusion-lang))
+Just fiddling around, 4th or 5th repo to try on exploring
 
-## Just fiddling around
+- Rust (finally decided to ditch c++)
+- compiler design (probably way beyond my intelligence(?))
+
+<!-- Following Julian Hartl's youtube series [_Building a compiler in rust_](https://github.com/julian-hartl/fusion-lang) -->
 
 1. trying to get the following expression parsed
 
--- src/parser.rs
+++ src/parser.rs
@@ -2,13 +2,22 @@
 use crate::lexer;
 
 #[derive(Debug)]
-pub struct Ast {
+pub struct Ast<'cu> {
+    cu: &'cu lexer::CompilationUnit,
     root: Option<AstNodeIndex>,
     nodes: AstNodes,
+    tokens: lexer::Tokens,
+}
+
+impl<'cu> Ast<'cu> {
+    fn get_str(&self, idx: AstNodeIndex) -> String {
+        self.nodes.get_str(self, idx)
+    }
 }
 
 #[derive(Debug)]
 enum AstNode {
+    EndOfLine(lexer::TokenIndex),
     Number(lexer::TokenIndex),
     BinaryOp {
         op: lexer::TokenIndex,
@@ -21,7 +30,28 @@ enum AstNode {
     },
 }
 
-#[derive(Debug)]
+impl AstNode {
+    fn get_str(&self, ast: &Ast) -> String {
+        match self {
+            AstNode::EndOfLine(token_idx) | AstNode::Number(token_idx) => {
+                ast.tokens.get(*token_idx).get_str(ast.cu).to_owned()
+            }
+            AstNode::BinaryOp { op, lhs, rhs } => {
+                let op_str = ast.tokens.get(*op).get_str(ast.cu);
+                let lhs_str = ast.get_str(*lhs);
+                let rhs_str = ast.get_str(*rhs);
+                format!("({} {} {})", lhs_str, op_str, rhs_str)
+            }
+            AstNode::UnaryOp { op, rhs } => {
+                let op_str = ast.tokens.get(*op).get_str(ast.cu);
+                let rhs_str = ast.get_str(*rhs);
+                format!("({} {})", op_str, rhs_str)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct AstNodeIndex(usize);
 
 impl AstNodeIndex {
@@ -43,13 +73,16 @@ impl AstNodes {
     }
 
     fn push(&mut self, node: AstNode) -> AstNodeIndex {
-        println!("pushing node: {:?}", node);
         self.0.push(node);
-        AstNodeIndex::new(self.0.len())
+        AstNodeIndex::new(self.0.len() - 1)
     }
 
     fn get(&self, idx: AstNodeIndex) -> &AstNode {
-        &self.0[idx.get()]
+        self.0.get(idx.get()).unwrap()
+    }
+
+    fn get_str(&self, ast: &Ast, idx: AstNodeIndex) -> String {
+        self.get(idx).get_str(ast)
     }
 }
 
@@ -123,16 +156,17 @@ mod precedence_climbing {
         ) -> Result<(Option<AstNode>, usize), ParseError> {
             let (mut lhs, mut cur_packed_token_idx) =
                 self.parse_primary(ast, tokens, cur_packed_token_idx)?;
-            if lhs.is_none() {
+            if lhs.is_none() || matches!(lhs, Some(AstNode::EndOfLine { .. })) {
                 return Ok((None, cur_packed_token_idx));
             }
 
             while cur_packed_token_idx < tokens.len() {
-                let op = tokens.get(cur_packed_token_idx);
-                if op.is_none() {
+                let Some(op) = tokens.get(cur_packed_token_idx) else {
                     break;
+                };
+                if op.1.get_kind() == lexer::TokenKind::SemiColon {
+                    return Ok((lhs, cur_packed_token_idx));
                 }
-                let op = op.unwrap();
 
                 let (precedence, associativity) = self
                     .get_precedence(&op.1)
@@ -171,11 +205,9 @@ mod precedence_climbing {
             tokens: &[(lexer::TokenIndex, lexer::Token)],
             mut cur_packed_token_idx: usize,
         ) -> Result<(Option<AstNode>, usize), ParseError> {
-            let token = tokens.get(cur_packed_token_idx);
-            if token.is_none() {
+            let Some(token) = tokens.get(cur_packed_token_idx) else {
                 return Ok((None, cur_packed_token_idx));
-            }
-            let token = token.unwrap();
+            };
 
             match token.1.get_kind() {
                 lexer::TokenKind::Int64 => {
@@ -215,9 +247,18 @@ mod precedence_climbing {
             }
         }
 
-        pub fn parse(&self, cu: &lexer::CompilationUnit) -> Result<Ast, ParseError> {
-            let (tokens, lex_errors): (Vec<_>, Vec<_>) = cu
-                .get_tokens()
+        pub fn parse<'cu>(&self, cu: &'cu lexer::CompilationUnit) -> Result<Ast<'cu>, ParseError> {
+            let tokens = cu.get_tokens();
+            let mut ast = Ast {
+                cu,
+                root: None,
+                nodes: AstNodes::new(tokens.len()),
+                tokens,
+            };
+
+            let (tokens, lex_errors): (Vec<_>, Vec<_>) = ast
+                .tokens
+                .clone()
                 .into_iter()
                 .enumerate()
                 .filter(|(_, token)| {
@@ -234,11 +275,6 @@ mod precedence_climbing {
                         .collect(),
                 ));
             }
-
-            let mut ast = Ast {
-                root: None,
-                nodes: AstNodes::new(tokens.len()),
-            };
 
             let (expr, _cur_packed_token_idx) =
                 self.parse_expression(&mut ast, &tokens, 0, 0).unwrap();
@@ -260,23 +296,41 @@ mod precedence_climbing {
         use super::*;
 
         #[test]
-        fn test_empty_input() {
-            let cu = lexer::CompilationUnit::from_string("stdin", "");
-            let parser = Parser::new();
-            let ast = parser.parse(&cu);
-            assert!(ast.is_ok());
-            let ast = ast.unwrap();
-            assert!(ast.root.is_none());
+        fn test_empty_ast() {
+            for s in ["", " ", ";", "\r\n\n;", "  ;", "\r\n\n"] {
+                let cu = lexer::CompilationUnit::from_string("stdin", s);
+                let parser = Parser::new();
+                let ast = parser.parse(&cu);
+                assert!(ast.is_ok());
+                assert!(
+                    matches!(ast, Ok(Ast { root: None, .. })),
+                    "input: `{}`, ast: {:?}",
+                    s,
+                    ast
+                );
+            }
         }
 
         #[test]
-        fn test_single_comma() {
-            let cu = lexer::CompilationUnit::from_string("stdin", ";");
-            let parser = Parser::new();
-            let ast = parser.parse(&cu);
-            assert!(ast.is_ok());
-            let ast = ast.unwrap();
-            assert!(ast.root.is_none());
+        fn test_one_number() {
+            for (s, expected) in [("0;", "0"), (" 0;", "0"), ("0 ;", "0"), (" 0 ;", "0")] {
+                let cu = lexer::CompilationUnit::from_string("stdin", s);
+                let parser = Parser::new();
+                let ast = parser.parse(&cu);
+                assert!(
+                    matches!(ast, Ok(Ast { root: Some(_), .. })),
+                    "failed on `{}`",
+                    s
+                );
+                let ast = ast.unwrap();
+                assert!(
+                    matches!(ast, Ast { root: Some(ast_node_idx), .. } if ast.get_str(ast_node_idx) == expected),
+                    "ast: {:?}, expected: `{}`, got: `{}`",
+                    ast,
+                    expected,
+                    ast.get_str(ast.root.unwrap())
+                );
+            }
         }
 
         #[test]
