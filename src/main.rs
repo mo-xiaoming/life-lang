f9a16cab2ab999d3e03bb055d54508a7a82149fd
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2023-12-07 23:06:15 +0800

    can calculate proper math expression

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use life_lang::{lexer, parser};
 fn main() {
     let cu = lexer::CompilationUnit::from_string(
         "stdin",
-        " 7^2 + 3 * (12 / (+15 / -( 3+1) - - 1) ) - 2^3^4",
+        " 7^2 + 3 * (12 / (15 / -( 3+1) - - 1) ) - 2^3^4",
     );
     let parser = parser::Parser::new();
     let ast = parser.parse(&cu).unwrap();
