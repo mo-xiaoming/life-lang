c31a528f4660d84ae3458154dee313aac915638e
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2023-12-11 18:22:43 +0800

    add string support (#15)

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,8 @@
 
 mod indices;
 
+use std::collections::HashMap;
+
 use indices::{ByteIndex, ByteIndexSpan, UcContentIndex};
 use unicode_segmentation::UnicodeSegmentation;
 
@@ -85,13 +87,15 @@ mod test_token_index {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, std::hash::Hash, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, std::hash::Hash, Clone)]
 pub(crate) enum TokenKind {
     Spaces,
     NewLine,
     SemiColon,
 
-    Int64,
+    I64,
+
+    StringLiteral { content: String },
 
     Plus,
     Dash,
@@ -102,7 +106,7 @@ pub(crate) enum TokenKind {
     LParen,
     RParen,
 
-    Invalid,
+    Invalid { msg: String },
 }
 
 impl std::fmt::Display for TokenKind {
@@ -111,7 +115,8 @@ impl std::fmt::Display for TokenKind {
             TokenKind::Spaces => write!(f, "Spaces"),
             TokenKind::NewLine => write!(f, "NewLine"),
             TokenKind::SemiColon => write!(f, "SemiColon"),
-            TokenKind::Int64 => write!(f, "Int64"),
+            TokenKind::I64 => write!(f, "I64"),
+            TokenKind::StringLiteral { content } => write!(f, "\"{}\"", content),
             TokenKind::Plus => write!(f, "Plus"),
             TokenKind::Dash => write!(f, "Dash"),
             TokenKind::Star => write!(f, "Star"),
@@ -119,7 +124,7 @@ impl std::fmt::Display for TokenKind {
             TokenKind::Percentage => write!(f, "Percentage"),
             TokenKind::LParen => write!(f, "LParen"),
             TokenKind::RParen => write!(f, "RParen"),
-            TokenKind::Invalid => write!(f, "Invalid"),
+            TokenKind::Invalid { msg } => write!(f, "{}", msg),
         }
     }
 }
@@ -140,8 +145,7 @@ mod test_token_kind {
     }
 
     test_display!(
-        Spaces, NewLine, SemiColon, Int64, Plus, Dash, Star, Slash, Percentage, LParen, RParen,
-        Invalid,
+        Spaces, NewLine, SemiColon, I64, Plus, Dash, Star, Slash, Percentage, LParen, RParen,
     );
 }
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -159,8 +163,8 @@ impl Token {
         self.span.get_str(cu)
     }
 
-    pub(crate) fn get_kind(&self) -> TokenKind {
-        self.kind
+    pub(crate) fn get_kind(&self) -> &TokenKind {
+        &self.kind
     }
 }
 
@@ -169,8 +173,8 @@ mod test_token {
     fn test_from() {
         use super::*;
         let span = ByteIndexSpan::new(ByteIndex::new(42), ByteIndex::new(47));
-        let token = Token::new(TokenKind::Invalid, span);
-        assert_eq!(token.kind, TokenKind::Invalid);
+        let token = Token::new(TokenKind::I64, span);
+        assert_eq!(token.kind, TokenKind::I64);
         assert_eq!(token.span, span);
     }
 
@@ -180,7 +184,7 @@ mod test_token {
         let s = "您好";
         let cu = CompilationUnit::from_string("mark", &format!("hi, {}", s));
         let token = Token::new(
-            TokenKind::Invalid,
+            TokenKind::I64,
             ByteIndexSpan::new(ByteIndex::new(4), ByteIndex::new(4 + s.len() - 1)),
         );
         assert_eq!(token.get_str(&cu), s);
@@ -411,6 +415,122 @@ impl CompilationUnit {
             start - 1
         }
 
+        fn take_string(
+            cu: &CompilationUnit,
+            mut start: UcContentIndex,
+        ) -> Result<(UcContentIndex, String), (UcContentIndex, String)> {
+            fn take_unicode(
+                cu: &CompilationUnit,
+                mut start: UcContentIndex,
+            ) -> Result<(UcContentIndex, String), (UcContentIndex, String)> {
+                let origin_start = start;
+
+                if let Some(byte_idx_span) = cu.ucs.try_to_byte_index_span(start) {
+                    let s = byte_idx_span.get_str(cu);
+                    if s != "{" {
+                        return Err((
+                            start,
+                            "unicode should be in the format of \\u{{...}}".to_owned(),
+                        ));
+                    }
+                } else {
+                    return Err((
+                        start,
+                        "unicode should be in the format of \\u{abcdef}".to_owned(),
+                    ));
+                };
+                start += 1;
+
+                while let Some(byte_idx_span) = cu.ucs.try_to_byte_index_span(start) {
+                    let s = byte_idx_span.get_str(cu);
+                    if s == "}" {
+                        break;
+                    }
+                    if s.len() != 1 || !s.chars().next().unwrap().is_ascii_hexdigit() {
+                        return Err((
+                            start,
+                            format!("only hex numbers are allowed in unicode sequence, `{}` is not allowed", s)
+                        ));
+                    }
+                    start += 1;
+                }
+
+                if start == origin_start + 1 {
+                    return Err((
+                        start,
+                        "unicode should be in the format of \\u{...}, cannot be empty between `{}`"
+                            .to_owned(),
+                    ));
+                }
+                let mut s = ByteIndexSpan::new(
+                    cu.ucs
+                        .try_to_byte_index_span(origin_start + 1)
+                        .unwrap()
+                        .get_start(),
+                    cu.ucs
+                        .try_to_byte_index_span(start - 1)
+                        .unwrap()
+                        .get_inclusive_end(),
+                )
+                .get_str(cu)
+                .to_owned();
+                if s.len() % 2 != 0 {
+                    s = format!("0{}", s);
+                }
+                let c = std::char::from_u32(u32::from_str_radix(&s, 16).unwrap_or_else(|e| {
+                    panic!("BUG: failed to parse `{}` as hex number: {}", s, e)
+                }))
+                .ok_or_else(|| {
+                    (
+                        start,
+                        format!("failed to convert `{}` to unicode character", s),
+                    )
+                })?;
+                Ok((start, c.to_string()))
+            }
+
+            let mut content = String::with_capacity(50);
+
+            let escaped_chars: HashMap<&str, &str> = [
+                ("\\", "\\"),
+                ("\"", "\""),
+                ("n", "\n"),
+                ("r", "\r"),
+                ("t", "\t"),
+                ("0", "\0"),
+            ]
+            .into_iter()
+            .collect();
+            let mut in_escape = false;
+            while let Some(byte_idx_span) = cu.ucs.try_to_byte_index_span(start) {
+                let s = byte_idx_span.get_str(cu);
+                if in_escape {
+                    if s == "u" {
+                        let (new_start, chunk) = take_unicode(cu, start + 1)?;
+                        start = new_start + 1;
+                        content.push_str(&chunk);
+                    } else if escaped_chars.contains_key(&s) {
+                        start += 1;
+                        content.push_str(escaped_chars[&s]);
+                    } else {
+                        return Err((start - 1, format!("unrecogonized escape `{}`", s)));
+                    }
+                    in_escape = false;
+                    continue;
+                } else if s == r#"""# {
+                    return Ok((start, content));
+                }
+                if s == "\\" {
+                    in_escape = true;
+                } else {
+                    content.push_str(s);
+                }
+                start += 1;
+            }
+
+            Err((start - 1, "unfinished string".to_owned()))
+        }
+
         fn get_single_char_token_kind(c: char) -> Option<TokenKind> {
             match c {
                 '+' => Some(TokenKind::Plus),
@@ -464,7 +584,15 @@ impl CompilationUnit {
             assert!(!s.is_empty());
             if s.len() != 1 {
                 let new_uc_idx = take_while(self, uc_idx + 1, |s| s.len() != 1);
-                tokens.push(TokenKind::Invalid, to_byte_range(self, uc_idx, new_uc_idx));
+                tokens.push(
+                    TokenKind::Invalid {
+                        msg: format!(
+                            "multi-char unicode like `{}` only supported in strings and comments",
+                            s
+                        ),
+                    },
+                    to_byte_range(self, uc_idx, new_uc_idx),
+                );
                 uc_idx = new_uc_idx + 1;
                 continue;
             }
@@ -484,16 +612,18 @@ impl CompilationUnit {
                 '0' => {
                     let new_uc_idx = take_while(self, uc_idx + 1, uc_is_ascii_digit);
                     let kind = if new_uc_idx == uc_idx {
-                        TokenKind::Int64
+                        TokenKind::I64
                     } else {
-                        TokenKind::Invalid
+                        TokenKind::Invalid {
+                            msg: "leading zero is not allowed".to_owned(),
+                        }
                     };
                     tokens.push(kind, to_byte_range(self, uc_idx, new_uc_idx));
                     uc_idx = new_uc_idx + 1;
                 }
                 '1'..='9' => {
                     let new_uc_idx = take_while(self, uc_idx + 1, uc_is_ascii_digit);
-                    tokens.push(TokenKind::Int64, to_byte_range(self, uc_idx, new_uc_idx));
+                    tokens.push(TokenKind::I64, to_byte_range(self, uc_idx, new_uc_idx));
                     uc_idx = new_uc_idx + 1;
                 }
                 ' ' => {
@@ -501,8 +631,24 @@ impl CompilationUnit {
                     tokens.push(TokenKind::Spaces, to_byte_range(self, uc_idx, new_uc_idx));
                     uc_idx = new_uc_idx + 1;
                 }
+                '"' => {
+                    let new_uc_idx = take_string(self, uc_idx + 1);
+                    let (kind, new_uc_idx) = match new_uc_idx {
+                        Ok((new_uc_idx, content)) => {
+                            (TokenKind::StringLiteral { content }, new_uc_idx)
+                        }
+                        Err((new_uc_idx, msg)) => (TokenKind::Invalid { msg }, new_uc_idx),
+                    };
+                    tokens.push(kind, to_byte_range(self, uc_idx, new_uc_idx));
+                    uc_idx = new_uc_idx + 1;
+                }
                 _ => {
-                    tokens.push(TokenKind::Invalid, to_byte_range(self, uc_idx, uc_idx));
+                    tokens.push(
+                        TokenKind::Invalid {
+                            msg: format!("unsupported {}", c),
+                        },
+                        to_byte_range(self, uc_idx, uc_idx),
+                    );
                     uc_idx += 1;
                 }
             }
@@ -613,29 +759,36 @@ mod test_compile_unit {
     }
 
     #[test]
+    fn test_lexing_empty_content() {
+        let cu = CompilationUnit::from_string("mark", "");
+        let tokens = cu.get_tokens();
+        assert_eq!(tokens.len(), 0, "{:?}", tokens);
+    }
+
+    #[test]
     fn test_get_tokens() {
         let cu = CompilationUnit::from_string("mark", "1 + 27 *  3 % (4 - -3);\n\r\n");
         let tokens = cu.get_tokens();
         let expected = [
-            (TokenKind::Int64, "1"),
+            (TokenKind::I64, "1"),
             (TokenKind::Spaces, " "),
             (TokenKind::Plus, "+"),
             (TokenKind::Spaces, " "),
-            (TokenKind::Int64, "27"),
+            (TokenKind::I64, "27"),
             (TokenKind::Spaces, " "),
             (TokenKind::Star, "*"),
             (TokenKind::Spaces, "  "),
-            (TokenKind::Int64, "3"),
+            (TokenKind::I64, "3"),
             (TokenKind::Spaces, " "),
             (TokenKind::Percentage, "%"),
             (TokenKind::Spaces, " "),
             (TokenKind::LParen, "("),
-            (TokenKind::Int64, "4"),
+            (TokenKind::I64, "4"),
             (TokenKind::Spaces, " "),
             (TokenKind::Dash, "-"),
             (TokenKind::Spaces, " "),
             (TokenKind::Dash, "-"),
-            (TokenKind::Int64, "3"),
+            (TokenKind::I64, "3"),
             (TokenKind::RParen, ")"),
             (TokenKind::SemiColon, ";"),
             (TokenKind::NewLine, "\n"),
@@ -649,9 +802,41 @@ mod test_compile_unit {
     }
 
     #[test]
-    fn test_lexing_empty_content() {
-        let cu = CompilationUnit::from_string("mark", "");
-        let tokens = cu.get_tokens();
-        assert_eq!(tokens.len(), 0, "{:?}", tokens);
+    fn test_lexing_empty_string() {
+        for s in [r#""""#, r#""abc""#] {
+            let cu = CompilationUnit::from_string("mark", s);
+            let tokens = cu.get_tokens();
+            assert_eq!(tokens.len(), 1, "{:?}", tokens);
+            let token = &tokens[TokenIndex::new(0)];
+            assert_eq!(token.get_str(&cu), s);
+            assert!(
+                matches!(token.get_kind(), TokenKind::StringLiteral { content } if format!(r#""{}""#, content) == s),
+                "{:?}",
+                token.get_kind(),
+            );
+        }
+    }
+
+    #[test]
+    fn test_escaped_string() {
+        for (input, expected_content) in [
+            (r#""ab \r \n xx\\ \"xx\t\0y""#, "ab \r \n xx\\ \"xx\t\0y"),
+            (
+                r#"" \u{41} x\u{4f60}xy{}\u{597d}a\u{1f198}""#,
+                " A x你xy{}好a🆘",
+            ),
+        ] {
+            let cu = CompilationUnit::from_string("mark", input);
+            let tokens = cu.get_tokens();
+            assert_eq!(tokens.len(), 1, "{:?}", tokens);
+            let token = &tokens[TokenIndex::new(0)];
+            assert_eq!(token.get_str(&cu), input);
+            assert!(
+                matches!(token.get_kind(), TokenKind::StringLiteral { content } if content == expected_content),
+                "expected: `{:?}`, got: `{:?}`",
+                expected_content,
+                token.get_kind(),
+            );
+        }
     }
 }
