ef98ccd305b7b4ba8367072eee3e98e2c2645ecc
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2024-08-04 15:35:19 +0800

    add coverage

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # life-lang compiler
 
+[![CI Build and Test](https://github.com/mo-xiaoming/life-lang/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/mo-xiaoming/life-lang/actions/workflows/build-and-test.yml)
+[![codecov](https://codecov.io/gh/mo-xiaoming/life-lang/branch/master/graph/badge.svg)](https://codecov.io/gh/mo-xiaoming/life-lang)
+
 ## How to build
 
 Make sure you have vcpkg installed. You can follow the official setup guide [here](https://github.com/microsoft/vcpkg#quick-start).
--- cmake/ProjectTools.cmake
+++ cmake/ProjectTools.cmake
@@ -21,21 +21,14 @@ if(ENABLE_CLANG_TIDY)
 endif()
 
 # Add coverage flags
-if(CMAKE_BUILD_TYPE STREQUAL "Coverage")
-    message(STATUS "Building with coverage information")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
-endif()
+option(ENABLE_COVERAGE "Enable coverage" OFF)
 
-# Custom target to generate coverage report
-if(CMAKE_BUILD_TYPE STREQUAL "Coverage")
-    add_custom_target(coverage
-        COMMAND ${CMAKE_COMMAND} -E make_directory coverage
-        COMMAND lcov --capture --directory . --output-file coverage/coverage.info
-        COMMAND lcov --remove coverage/coverage.info '/usr/*' --output-file coverage/coverage.info
-        COMMAND lcov --list coverage/coverage.info
-        COMMAND genhtml coverage/coverage.info --output-directory coverage
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        COMMENT "Generating coverage report"
-    )
+if(ENABLE_COVERAGE)
+    if(CMAKE_COMPILER_IS_GNUCXX)
+        message(STATUS "Building with coverage information")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
+    else()
+        message(FATAL_ERROR "Coverage is only supported with GCC")
+    endif()
 endif()
