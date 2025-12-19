2d03b464eb1da9b84e67efcd02a99e037b9b1386
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2025-12-19 14:33:10 +0800

    fix test clang-tidy

--- tests/integration/test_bitwise_ops.cpp
+++ tests/integration/test_bitwise_ops.cpp
@@ -7,27 +7,37 @@ TEST_CASE("Bitwise AND in expressions") {
     life_lang::parser::Parser parser("flags & mask");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary & (var ((var_segment \"flags\"))) (var ((var_segment \"mask\"))))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary & (var ((var_segment \"flags\"))) (var ((var_segment \"mask\"))))"
+        );
+    }
+    }
   }
 
   SUBCASE("bitwise AND with hex literals") {
     life_lang::parser::Parser parser("0xFF & 0x0F");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary & (integer \"0xFF\") (integer \"0x0F\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary & (integer \"0xFF\") (integer \"0x0F\"))");
+    }
+    }
   }
 
   SUBCASE("bitwise AND in let statement") {
     life_lang::parser::Parser parser("let result = value & 0xFF;");
     auto const stmt = parser.parse_statement();
     REQUIRE(stmt.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*stmt, 0) ==
-        "(let false (pattern \"result\") nil (binary & (var ((var_segment \"value\"))) (integer \"0xFF\")))"
-    );
+    if (stmt.has_value()) {
+      CHECK(
+          life_lang::ast::to_sexp_string(*stmt, 0) ==
+          "(let false (pattern \"result\") nil (binary & (var ((var_segment \"value\"))) (integer \"0xFF\")))"
+      );
+    }
   }
 }
 
@@ -36,31 +46,41 @@ TEST_CASE("Bitwise OR in expressions") {
     life_lang::parser::Parser parser("a | b");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) == "(binary | (var ((var_segment \"a\"))) (var ((var_segment \"b\"))))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) == "(binary | (var ((var_segment \"a\"))) (var ((var_segment \"b\"))))"
+        );
+    }
+    }
   }
 
   SUBCASE("bitwise OR with binary literals") {
     life_lang::parser::Parser parser("0b0001 | 0b0010 | 0b0100");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary | (binary | (integer \"0b0001\") (integer \"0b0010\")) (integer \"0b0100\"))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary | (binary | (integer \"0b0001\") (integer \"0b0010\")) (integer \"0b0100\"))"
+        );
+    }
+    }
   }
 
   SUBCASE("permission flags combination") {
     life_lang::parser::Parser parser("let perms = READ | WRITE | EXECUTE;");
     auto const stmt = parser.parse_statement();
     REQUIRE(stmt.has_value());
-    auto const sexp = life_lang::ast::to_sexp_string(*stmt, 0);
-    CHECK(
-        sexp ==
-        "(let false (pattern \"perms\") nil (binary | (binary | (var ((var_segment \"READ\"))) (var ((var_segment "
-        "\"WRITE\")))) (var ((var_segment \"EXECUTE\")))))"
-    );
+    if (stmt.has_value()) {
+      auto const sexp = life_lang::ast::to_sexp_string(*stmt, 0);
+      CHECK(
+          sexp ==
+          "(let false (pattern \"perms\") nil (binary | (binary | (var ((var_segment \"READ\"))) (var ((var_segment "
+          "\"WRITE\")))) (var ((var_segment \"EXECUTE\")))))"
+      );
+    }
   }
 }
 
@@ -69,16 +89,24 @@ TEST_CASE("Bitwise XOR in expressions") {
     life_lang::parser::Parser parser("a ^ b");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) == "(binary ^ (var ((var_segment \"a\"))) (var ((var_segment \"b\"))))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) == "(binary ^ (var ((var_segment \"a\"))) (var ((var_segment \"b\"))))"
+        );
+    }
+    }
   }
 
   SUBCASE("XOR with hex literals") {
     life_lang::parser::Parser parser("0xFF ^ 0xAA");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary ^ (integer \"0xFF\") (integer \"0xAA\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary ^ (integer \"0xFF\") (integer \"0xAA\"))");
+    }
+    }
   }
 }
 
@@ -87,25 +115,35 @@ TEST_CASE("Shift left operations") {
     life_lang::parser::Parser parser("value << 2");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary << (var ((var_segment \"value\"))) (integer \"2\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary << (var ((var_segment \"value\"))) (integer \"2\"))");
+    }
+    }
   }
 
   SUBCASE("shift left with literal") {
     life_lang::parser::Parser parser("1 << 8");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary << (integer \"1\") (integer \"8\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary << (integer \"1\") (integer \"8\"))");
+    }
+    }
   }
 
   SUBCASE("shift left in let statement") {
     life_lang::parser::Parser parser("let shifted = bits << count;");
     auto const stmt = parser.parse_statement();
     REQUIRE(stmt.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*stmt, 0) ==
-        "(let false (pattern \"shifted\") nil (binary << (var ((var_segment \"bits\"))) (var ((var_segment "
-        "\"count\")))))"
-    );
+    if (stmt.has_value()) {
+      CHECK(
+          life_lang::ast::to_sexp_string(*stmt, 0) ==
+          "(let false (pattern \"shifted\") nil (binary << (var ((var_segment \"bits\"))) (var ((var_segment "
+          "\"count\")))))"
+      );
+    }
   }
 }
 
@@ -114,14 +152,22 @@ TEST_CASE("Shift right operations") {
     life_lang::parser::Parser parser("value >> 4");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary >> (var ((var_segment \"value\"))) (integer \"4\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary >> (var ((var_segment \"value\"))) (integer \"4\"))");
+    }
+    }
   }
 
   SUBCASE("shift right with hex") {
     life_lang::parser::Parser parser("0xFF00 >> 8");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary >> (integer \"0xFF00\") (integer \"8\"))");
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(life_lang::ast::to_sexp_string(*expr, 0) == "(binary >> (integer \"0xFF00\") (integer \"8\"))");
+    }
+    }
   }
 }
 
@@ -130,41 +176,57 @@ TEST_CASE("Complex bitwise expressions") {
     life_lang::parser::Parser parser("(value & 0xFF) << 8");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary << (binary & (var ((var_segment \"value\"))) (integer \"0xFF\")) (integer \"8\"))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary << (binary & (var ((var_segment \"value\"))) (integer \"0xFF\")) (integer \"8\"))"
+        );
+    }
+    }
   }
 
   SUBCASE("combine bytes") {
     life_lang::parser::Parser parser("(high << 8) | low");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary | (binary << (var ((var_segment \"high\"))) (integer \"8\")) (var ((var_segment \"low\"))))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary | (binary << (var ((var_segment \"high\"))) (integer \"8\")) (var ((var_segment \"low\"))))"
+        );
+    }
+    }
   }
 
   SUBCASE("RGB color composition") {
     life_lang::parser::Parser parser("(r << 16) | (g << 8) | b");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary | (binary | (binary << (var ((var_segment \"r\"))) (integer \"16\")) (binary << (var ((var_segment "
-        "\"g\"))) (integer \"8\"))) (var ((var_segment \"b\"))))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary | (binary | (binary << (var ((var_segment \"r\"))) (integer \"16\")) (binary << (var ((var_segment "
+            "\"g\"))) (integer \"8\"))) (var ((var_segment \"b\"))))"
+        );
+    }
+    }
   }
 
   SUBCASE("extract byte from word") {
     life_lang::parser::Parser parser("(word >> 8) & 0xFF");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary & (binary >> (var ((var_segment \"word\"))) (integer \"8\")) (integer \"0xFF\"))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary & (binary >> (var ((var_segment \"word\"))) (integer \"8\")) (integer \"0xFF\"))"
+        );
+    }
+    }
   }
 }
 
@@ -177,8 +239,12 @@ TEST_CASE("Bitwise operators in match expressions") {
     })");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    auto const sexp = life_lang::ast::to_sexp_string(*expr, 0);
-    CHECK(sexp.find("(binary & (var ((var_segment \"flags\"))) (integer \"0x07\"))") != std::string::npos);
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        auto const sexp = life_lang::ast::to_sexp_string(*expr, 0);
+        CHECK(sexp.find("(binary & (var ((var_segment \"flags\"))) (integer \"0x07\"))") != std::string::npos);
+    }
+    }
   }
 }
 
@@ -187,20 +253,26 @@ TEST_CASE("Bitwise operators with binary literals") {
     life_lang::parser::Parser parser("let flags = 0b0001 | 0b0100 | 0b1000;");
     auto const stmt = parser.parse_statement();
     REQUIRE(stmt.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*stmt, 0) ==
-        "(let false (pattern \"flags\") nil (binary | (binary | (integer \"0b0001\") (integer \"0b0100\")) (integer "
-        "\"0b1000\")))"
-    );
+    if (stmt.has_value()) {
+      CHECK(
+          life_lang::ast::to_sexp_string(*stmt, 0) ==
+          "(let false (pattern \"flags\") nil (binary | (binary | (integer \"0b0001\") (integer \"0b0100\")) (integer "
+          "\"0b1000\")))"
+      );
+    }
   }
 
   SUBCASE("masking with binary literals") {
     life_lang::parser::Parser parser("value & 0b11110000");
     auto const expr = parser.parse_expr();
     REQUIRE(expr.has_value());
-    CHECK(
-        life_lang::ast::to_sexp_string(*expr, 0) ==
-        "(binary & (var ((var_segment \"value\"))) (integer \"0b11110000\"))"
-    );
+    if (expr.has_value()) {
+      if (expr.has_value()) {
+        CHECK(
+            life_lang::ast::to_sexp_string(*expr, 0) ==
+            "(binary & (var ((var_segment \"value\"))) (integer \"0b11110000\"))"
+        );
+    }
+    }
   }
 }
--- tests/integration/test_string_interpolation.cpp
+++ tests/integration/test_string_interpolation.cpp
@@ -13,80 +13,98 @@ TEST_CASE("String interpolation - simple variable") {
   Parser parser(R"("Hello, {name}!")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Hello, "), var_name("name"), string_part("!")});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Hello, "), var_name("name"), string_part("!")});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation - multiple variables") {
   Parser parser(R"xxx("Point: ({x}, {y})")xxx");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected =
-      string_interp({string_part("Point: ("), var_name("x"), string_part(", "), var_name("y"), string_part(")")});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected =
+        string_interp({string_part("Point: ("), var_name("x"), string_part(", "), var_name("y"), string_part(")")});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation - expression") {
   Parser parser(R"("Result: {x + y}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Result: "), binary_expr("+", var_name("x"), var_name("y"))});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Result: "), binary_expr("+", var_name("x"), var_name("y"))});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation - field access") {
   Parser parser(R"("Name: {user.name}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Name: "), field_access(var_name("user"), "name")});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Name: "), field_access(var_name("user"), "name")});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation - function call") {
   Parser parser(R"("Value: {get_value()}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Value: "), function_call(var_name("get_value"), {})});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Value: "), function_call(var_name("get_value"), {})});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation - method chain") {
   Parser parser(R"("Upper: {name.to_upper()}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Upper: "), function_call(var_name_path({"name", "to_upper"}), {})});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Upper: "), function_call(var_name_path({"name", "to_upper"}), {})});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("Empty braces - not interpolation") {
   Parser parser(R"("Format: {}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  CHECK(to_sexp_string(*expr, 0) == R"((string "\"Format: {}\""))");
+    CHECK(to_sexp_string(*expr, 0) == R"((string "\"Format: {}\""))");
+  }
 }
 
 TEST_CASE("Escaped braces - literal") {
   Parser parser(R"("Literal: \{value\}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  CHECK(to_sexp_string(*expr, 0) == R"((string "\"Literal: \\{value\\}\""))");
+    CHECK(to_sexp_string(*expr, 0) == R"((string "\"Literal: \\{value\\}\""))");
+  }
 }
 
 TEST_CASE("Mixed escaped and interpolated") {
   Parser parser(R"("Literal \{x\}, interpolated {y}")");
   auto const expr = parser.parse_expr();
   REQUIRE(expr.has_value());
+  if (expr.has_value()) {
 
-  auto const expected = string_interp({string_part("Literal \\{x\\}, interpolated "), var_name("y")});
-  CHECK(to_sexp_string(*expr, 0) == expected);
+    auto const expected = string_interp({string_part("Literal \\{x\\}, interpolated "), var_name("y")});
+    CHECK(to_sexp_string(*expr, 0) == expected);
+  }
 }
 
 TEST_CASE("String interpolation in function") {
@@ -99,5 +117,7 @@ fn greet(name: String): String {
   Parser parser(k_input);
   auto const module = parser.parse_module();
   REQUIRE(module);
-  CHECK(module->items.size() == 1);
+  if (module) {
+    CHECK(module->items.size() == 1);
+  }
 }
